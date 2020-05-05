df0cf694d9a87a760c789bc55ba445c60e3c1ee0
juan jardim <juanjardim@gmail.com>
2020-05-05 18:54:22 +0200

    Implement main functionalities

--- js/display.js
+++ js/display.js
@@ -1,10 +1,78 @@
 
+const availableIcons = ['tree', 'map-signs', 'route', 'toilet-paper', 'binoculars', 'fire', 'frog', 'hiking', 'faucet', 'caravan'];
+const numberOfCardPerRow = 4;
+function createCard(index) {
+    let card = document.createElement('div');
+    card.setAttribute('class', 'col-5 col-sm-6 col-md-4 col-lg-3 memory-card question');
+    card.setAttribute('data-index', `00-${index}`);
+    return card;
+}
+function createRow(){
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row justify-content-between');
+    return row;
+}
+
+function createRandomArray(arrayItems, maxNumber, availableIconsCount){
+    if(arrayItems.length == maxNumber){
+        return arrayItems;
+    }
+
+    let index = Math.floor(Math.random() * availableIconsCount + 1);
+
+    if(arrayItems.indexOf(index) < 0){
+        arrayItems.push(index);
+    }
+
+    return createRandomArray(arrayItems, maxNumber, availableIconsCount);
+}
+
+
+function shuffleCards(numberOfCards) {
+    let cards = [];
+    let half = numberOfCards / 2;
+
+    let randomItems = createRandomArray([], half, availableIcons.length - 1);
+
+    
+    for(let i = 0; i < half; i++) {
+        let index = randomItems[i];
+        cards.push(availableIcons[index]);
+        cards.push(availableIcons[index]);
+    }
+
+    return cards.sort(() => Math.random() - 0.5);;
+    
+}
+
+function setCardContent(numberOfCards) {
+    let cardContent = shuffleCards(numberOfCards);
+    var cards = document.querySelectorAll('.memory-card');
+
+    let i = 0;
+    for(let card of cards) {
+        let content = document.createElement('i');
+        content.setAttribute('class', `fas fa-${cardContent[i]}`);
+        i++;
+        card.appendChild(content);
+    }
+
+}
 
 function createGrid(numberOfCards, containerId){
+    if(numberOfCards % numberOfCardPerRow !== 0)
+        throw 'Invalid number of cards';
+
     const container = document.querySelector(`#${containerId}`);
     container.innerHTML = '';
+    let row = createRow();
+    
+    for(let i = 0; i < numberOfCards; i++) {
+        row.appendChild(createCard(i));
+    }
 
-
+    container.appendChild(row);
+    setCardContent(numberOfCards);    
 }
 
 export {createGrid as default};
--- js/main.js
+++ js/main.js
@@ -2,13 +2,61 @@ import createGrid from './display.js'
 
 const numberOfCards = 12;
 const cardContainerId = 'cardContainer';
+let lastElementSelected = null;
+let isBlock = false;
+
+function flipCard(event) {
+    if(isBlock) {
+        return;
+    }
+    let targetElement = event.currentTarget;           
+
+    if(targetElement.classList.contains('valid')) {
+        return;
+    }
+
+    if(lastElementSelected === null) {
+        targetElement.classList.remove('question');
+        lastElementSelected = targetElement;
+        return;
+    }
+
+    if(targetElement.getAttribute('data-index') == lastElementSelected.getAttribute('data-index')) {
+        targetElement.classList.add('question');
+        lastElementSelected = null;
+        return;
+    }
+    
+    targetElement.classList.remove('question');
+
+    let elementContent = targetElement.querySelector('.fas');
+    let lastElementSelectedContent = lastElementSelected.querySelector('.fas');
+    if(lastElementSelectedContent.classList.value != elementContent.classList.value) {
+        isBlock = true;
+        setTimeout(() =>{
+            lastElementSelected.classList.add('question');
+            targetElement.classList.add('question');
+            lastElementSelected = null;
+            isBlock = false;
+        },500);
+    } else {
+        lastElementSelected.classList.add('valid');
+        targetElement.classList.add('valid');
+        lastElementSelected = null;
+    }
+}
+
+function bindOnClickCard() {
+    let cards = document.querySelectorAll('.memory-card');
+    for(let card of cards) {
+        card.addEventListener('click', flipCard);
+    }
+}
 
 (() => {
     document.querySelector('#startGameBtn').addEventListener('click', () => {
+        lastElementSelected = null;
         createGrid(numberOfCards, cardContainerId);
+        bindOnClickCard();
     });
-
-    document.querySelector('.memory-card').addEventListener('click', () => {
-        console.log('clicked');
-    })
 })()
